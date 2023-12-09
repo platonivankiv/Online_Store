21cc3c2a03e6de984f1f55ec1e0fe7f0c9abf884
platon.ivankiv <ivankiv.platon@gmail.com>
2023-12-09 16:05:22 +0300

    Creating a product page

--- src/components/Products/SingleProducts.jsx
+++ src/components/Products/SingleProducts.jsx
@@ -2,13 +2,19 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { ROUTES } from "../../utils/routes";
-import {useGetProductQuery} from "../../features/api/apiSlice";
+import { useGetProductQuery } from "../../features/api/apiSlice";
+import { getRelatedProducts } from "../../features/products/productsSlice";
 
+import { ROUTES } from "../../utils/routes";
+
+import Product from "./Product";
+import Products from "./Products";
 
 const SingleProduct = () => {
+    const dispatch = useDispatch();
     const { id } = useParams();
     const navigate = useNavigate();
+    const { list, related } = useSelector(({ products }) => products);
 
     const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id });
 
@@ -19,7 +25,20 @@ const SingleProduct = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isLoading, isFetching, isSuccess]);
 
-    return <div>SingleProduct</div>
+    useEffect(() => {
+        if (!data || !list.length) return;
+
+        dispatch(getRelatedProducts(data.category.id));
+    }, [data, dispatch, list.length]);
+
+    return !data ? (
+        <section className="preloader">Loading...</section>
+    ) : (
+        <>
+            <Product {...data} />
+            <Products products={related} amount={5} title="Related products" />
+        </>
+    );
 };
 
 
--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,6 +1,8 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import axios from "axios";
+
 import {BASE_URL} from "../../utils/constants";
+import {shuffle} from "../../utils/common";
 
 export const getProducts = createAsyncThunk(
     "products/getProducts",
@@ -8,41 +10,44 @@ export const getProducts = createAsyncThunk(
         try {
             const res = await axios(`${BASE_URL}/products`);
             return res.data;
-
         } catch (err) {
+            console.log(err);
             return thunkAPI.rejectWithValue(err);
         }
-    },
+    }
 );
 
-
-    const productsSlice = createSlice({
-        name: "products",
-        initialState: {
-            list: [],
-            filtered: [],
-            // related: [],
-            isLoading: false,
+const productsSlice = createSlice({
+    name: "products",
+    initialState: {
+        list: [],
+        filtered: [],
+        related: [],
+        isLoading: false,
+    },
+    reducers: {
+        filterByPrice: (state, {payload}) => {
+            state.filtered = state.list.filter(({price}) => price < payload);
         },
-        reducers: {
-            filterByPrice: (state, {payload}) => {
-                state.filtered = state.list.filter(({price}) => price < payload);
-            },
+        getRelatedProducts: (state, {payload}) => {
+            const list = state.list.filter(({category: {id}}) => id === payload);
+            state.related = shuffle(list);
         },
-            extraReducers: (builder) => {
-                builder.addCase(getProducts.pending, (state) => {
-                    state.isLoading = true;
-                });
-                builder.addCase(getProducts.fulfilled, (state, {payload}) => {
-                    state.list = payload;
-                    state.isLoading = false;
-                });
-                builder.addCase(getProducts.rejected, (state) => {
-                    state.isLoading = false;
-                });
-            },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getProducts.pending, (state) => {
+            state.isLoading = true;
         });
+        builder.addCase(getProducts.fulfilled, (state, {payload}) => {
+            state.list = payload;
+            state.isLoading = false;
+        });
+        builder.addCase(getProducts.rejected, (state) => {
+            state.isLoading = false;
+        });
+    },
+});
 
-export const { filterByPrice} = productsSlice.actions
+export const {filterByPrice, getRelatedProducts} = productsSlice.actions;
 
 export default productsSlice.reducer;
