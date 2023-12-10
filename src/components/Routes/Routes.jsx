e59dcee40877acb9bc0e444c4fe39f8fc4061496
platon.ivankiv <ivankiv.platon@gmail.com>
2023-12-10 14:25:12 +0300

    Components for a specific category

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
-import Home from "../Home/Home";
+
 import {ROUTES} from "../../utils/routes";
+
+import Home from "../Home/Home";
 import SingleProducts from "../Products/SingleProducts";
 import Profile from "../Profile/Profile";
+import SingleCategory from "../Categories/SingleCategory";
 
 const AppRoutes = () => (
-        <Routes>
-            <Route index element={<Home />}/>
-            <Route path={ROUTES.PRODUCT} element={<SingleProducts />}/>
-            <Route path={ROUTES.PROFILE} element={<Profile />}/>
-        </Routes>
-    );
+    <Routes>
+        <Route index element={<Home/>}/>
+        <Route path={ROUTES.PRODUCT} element={<SingleProducts/>}/>
+        <Route path={ROUTES.PROFILE} element={<Profile/>}/>
+        <Route path={ROUTES.CATEGORY} element={<SingleCategory/>}/>
+    </Routes>
+);
 
 
 export default AppRoutes;
